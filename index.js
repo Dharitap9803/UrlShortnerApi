d3d53101fe1cc44751eb153a9ff5b444c763bd2c
Dharitap9803 <dharitapatel.21.cs@iite.indusuni.ac.in>
2026-02-03 22:55:20 +0530

    adding files

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const { connectToMongoDB } = require('./connect')
+const urlRoute = require("./routes/url");
+
+const app = express();
+const PORT = 8001;
+
+connectToMongoDB('mongodb://localhost:27017/urlShortenerDB')
+.then(() => console.log("Mongodb connected"))
+
+app.use("/url", urlRoute);
+
+app.listen(PORT, () => console.log(`Server running at PORT:${PORT}`));
