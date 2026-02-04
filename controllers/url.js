37080449286520408ddf51abefe555d4eaeadd2d
Dharitap9803 <dharitapatel.21.cs@iite.indusuni.ac.in>
2026-02-04 16:23:31 +0530

    uploading the changes been made

--- controllers/url.js
+++ controllers/url.js
@@ -1,20 +1,41 @@
 const shortid = require("shortid");
-const URL =require('../models/url');
+const URL = require('../models/url');
 
 async function handleGenerateNewShortURL(req, res) {
     const body = req.body;
-    if(!body.url) return res.status(400).json({error})
+
+    if(!body.url) return res.status(400).json({ error: "URL is required" });
+
+    if (
+        !body.url.startsWith("http://") &&
+        !body.url.startsWith("https://")
+    ) {
+        body.url = "https://" + body.url;
+    }
+
     const shortID = shortid();
-    await URL.create({shortId: shortID,
-        redirectURL:  body.url,
+
+    await URL.create({
+        shortId: shortID,
+        redirectURL: body.url,
         visitHistory: [],
     });
 
-    return res.json({ id: shortID});
-
+    return res.json({ id: shortID });
 }
 
+async function handleGetAnalytics(req, res){
+    const shortId = req.params.shortId;
+
+    const result = await URL.findOne({ shortId });
+
+    return res.json({ 
+        totalClicks: result.visitHistory.length, 
+        analytics: result.visitHistory,
+    });
+}
 
 module.exports = {
     handleGenerateNewShortURL,
+    handleGetAnalytics,
 };
--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 const express = require("express");
-
-const { connectToMongoDB } = require('./connect')
+const URL = require('./models/url');
+const { connectToMongoDB } = require('./connect');
 const urlRoute = require("./routes/url");
 
 const app = express();
 const PORT = 8001;
 
 connectToMongoDB('mongodb://localhost:27017/urlShortenerDB')
-.then(() => console.log("Mongodb connected"))
+.then(() => console.log("Mongodb connected"));
 
+app.use(express.json());
 app.use("/url", urlRoute);
 
+// Redirect Route
+app.get('/:shortId', async (req, res) => {
+    const { shortId } = req.params;
+
+    const entry = await URL.findOneAndUpdate(
+        { shortId },
+        {
+            $push: {
+                visitHistory: {
+                    timestamp: Date.now(),
+                },
+            },
+        },
+        { new: true }
+    );
+
+    if (!entry) {
+        return res.status(404).json({ error: "Short URL not found" });
+    }
+
+    let redirectURL = entry.redirectURL;
+
+    if (
+        !redirectURL.startsWith("http://") &&
+        !redirectURL.startsWith("https://")
+    ) {
+        redirectURL = "https://" + redirectURL;
+    }
+
+    res.redirect(redirectURL);
+});
+
 app.listen(PORT, () => console.log(`Server running at PORT:${PORT}`));
+
